6046474e15a0c4bfda7d75009f193471bc9e5d89
Pandicon <70060103+Pandicon@users.noreply.github.com>
2022-12-03 22:23:30 +0100

    Add support for value ranges and errors

--- src/get_starting_values.rs
+++ src/get_starting_values.rs
@@ -1,40 +1,62 @@
 use std::io::Write;
 
-pub fn get_starting_values(mut args: Vec<String>, starting_vals_names: &[&str]) -> (Vec<[f64; 2]>, [f64; 3]) {
-	let mut starting_vals = [0.0, 0.0, 0.0];
+use crate::structs::{Parameter, Point};
+
+pub fn get_starting_values(mut args: Vec<String>) -> (Vec<Point>, [Parameter; 3]) {
+	let mut starting_vals = [
+		Parameter::default_with_name("A".to_string()),
+		Parameter::default_with_name("B".to_string()),
+		Parameter::default_with_name("F".to_string()),
+	];
 	let mut i = 0;
 	let mut ask_for_starting_vals = true;
-	while !args.is_empty() {
-		let val = args.remove(0);
-		if val.trim() == "|" {
-			ask_for_starting_vals = false;
-			break;
-		}
-		if let Ok(x) = val.parse::<f64>() {
-			starting_vals[i] = x;
-			println!("Using {} = {} from arguments", starting_vals_names[i], x);
-			i += 1;
-			if i == starting_vals.len() {
-				break;
+	'outer: while !args.is_empty() {
+		let mut vals = vec![];
+		for _ in 0..4 {
+			if args.is_empty() {
+				break 'outer;
 			}
+			let val = args.remove(0);
+			if val.trim() == "|" {
+				ask_for_starting_vals = false;
+				break 'outer;
+			}
+			match val.parse::<f64>() {
+				Ok(v) => vals.push(v),
+				Err(_) => {
+					continue 'outer;
+				}
+			}
+		}
+		starting_vals[i].value = vals[0];
+		starting_vals[i].min = vals[1];
+		starting_vals[i].max = vals[2];
+		starting_vals[i].steps = vals[3];
+		println!(
+			"Using {} = {}; {} <= {} <= {}; steps = {} from arguments",
+			starting_vals[i].name, starting_vals[i].value, starting_vals[i].min, starting_vals[i].name, starting_vals[i].max, starting_vals[i].steps
+		);
+		i += 1;
+		if i == starting_vals.len() {
+			break;
 		}
 	}
 	while i < starting_vals.len() && ask_for_starting_vals {
-		print!("What should the starting value of {} be? ", starting_vals_names[i]);
+		print!("What should the starting value of {} be? ", starting_vals[i].name);
 		if std::io::stdout().flush().is_err() {
 			println!();
 		};
-		let line = read_input_line();
-		match line.parse::<f64>() {
-			Ok(x) => {
-				starting_vals[i] = x;
-				println!("Using {} = {}", starting_vals_names[i], x);
-			}
-			Err(err) => println!("Failed to parse the input value: {}", err),
+		let line = read_input_line().trim().split(' ').filter_map(|x| x.parse().ok()).collect::<Vec<f64>>();
+		if line.len() < 4 {
+			continue;
 		}
+		starting_vals[i].value = line[0];
+		starting_vals[i].min = line[1];
+		starting_vals[i].max = line[2];
+		starting_vals[i].steps = line[3];
 		i += 1;
 	}
-	let mut points: Vec<[f64; 2]> = vec![];
+	let mut points: Vec<Point> = vec![];
 	let mut ask_for_points = true;
 	while !args.is_empty() {
 		let val = args.remove(0);
@@ -50,13 +72,22 @@ pub fn get_starting_values(mut args: Vec<String>, starting_vals_names: &[&str]) 
 					break;
 				}
 				if let Ok(y) = val.parse() {
-					points.push([x, y]);
+					if !args.is_empty() {
+						let val = args.remove(0);
+						if val.trim() == "|" {
+							ask_for_points = false;
+							break;
+						}
+						if let Ok(error) = val.parse() {
+							points.push(Point::from_array([x, y, error]));
+						}
+					}
 				}
 			}
 		}
 	}
 	if ask_for_points {
-		println!("Input [x, y] pairs to include to the points one by one, separated with new lines ('s' to stop): ");
+		println!("Input [x, y, error] pairs to include to the points one by one, separated with new lines ('s' to stop): ");
 	}
 	if ask_for_points {
 		loop {
@@ -68,21 +99,24 @@ pub fn get_starting_values(mut args: Vec<String>, starting_vals_names: &[&str]) 
 				break;
 			}
 			let vals = line.split(' ').filter_map(|x| x.trim().parse::<f64>().ok()).collect::<Vec<f64>>();
-			if vals.len() < 2 {
+			if vals.len() < 3 {
 				continue;
 			}
-			points.push([vals[0], vals[1]]);
+			points.push(Point::from_array([vals[0], vals[1], vals[2]]));
 		}
 	}
 	println!();
 	println!("--------------------");
-	for (name, value) in starting_vals_names.iter().zip(starting_vals) {
-		println!("Using {} = {}", name, value);
+	for parameter in &starting_vals {
+		println!(
+			"Using {} = {}; {} <= {} <= {}; steps = {}",
+			parameter.name, parameter.value, parameter.min, parameter.name, parameter.max, parameter.steps
+		);
 	}
 	println!("----------");
 	println!("Using the following points:");
-	for [x, y] in &points {
-		println!("[{x}, {y}]");
+	for point in &points {
+		println!("[{}, {}]", point.x, point.y);
 	}
 	println!("--------------------");
 	(points, starting_vals)
