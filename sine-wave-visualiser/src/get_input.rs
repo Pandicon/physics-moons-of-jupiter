521b76a1d29098e4ce3090cf8f9be52f73a3baa3
Pandicon <70060103+Pandicon@users.noreply.github.com>
2022-12-06 22:31:19 +0100

    (Waves visualiser) Fix an issue with loading the input
    It would previously break out of the outer loop when it encountered "-----" without pushing the data

--- sine-wave-visualiser/src/get_input.rs
+++ sine-wave-visualiser/src/get_input.rs
@@ -2,7 +2,8 @@ use crate::structs::{InputSection, Point, Wave};
 
 pub fn get_input() -> Vec<InputSection> {
 	let mut data = vec![];
-	'outer: loop {
+	let mut loop_outer = true;
+	while loop_outer {
 		let mut points = vec![];
 		let mut waves = vec![];
 		loop {
@@ -10,7 +11,8 @@ pub fn get_input() -> Vec<InputSection> {
 			if line == *"---" {
 				break;
 			} else if line == *"-----" {
-				break 'outer;
+				loop_outer = false;
+				break;
 			}
 			let split = line.split(' ').filter_map(|x| x.parse::<f64>().ok()).collect::<Vec<f64>>();
 			if let Some(point) = Point::from_vec(split) {
@@ -20,22 +22,24 @@ pub fn get_input() -> Vec<InputSection> {
 				points.push(point)
 			}
 		}
-		loop {
-			let line = read_input_line();
-			if line == *"---" {
-				break;
-			} else if line == *"-----" {
-				break 'outer;
-			}
-			let split = line.split(' ').filter_map(|x| x.parse::<f64>().ok()).collect::<Vec<f64>>();
-			if let Some(wave) = Wave::from_vec(split) {
-				waves.push(wave)
+		if loop_outer {
+			loop {
+				let line = read_input_line();
+				if line == *"---" {
+					break;
+				} else if line == *"-----" {
+					loop_outer = false;
+					break;
+				}
+				let split = line.split(' ').filter_map(|x| x.parse::<f64>().ok()).collect::<Vec<f64>>();
+				if let Some(wave) = Wave::from_vec(split) {
+					waves.push(wave)
+				}
 			}
 		}
 		let points_x = points.iter().map(|point| point.x);
 		let mut points_min_x = None;
 		let mut points_max_x = None;
-		println!("{:?}", points_x);
 		for x in points_x {
 			if let Some(min_x) = points_min_x {
 				if x < min_x {
@@ -52,7 +56,6 @@ pub fn get_input() -> Vec<InputSection> {
 				points_max_x = Some(x)
 			}
 		}
-		println!("{:?} {:?}", points_min_x, points_max_x);
 		data.push(InputSection {
 			points_min_x: points_min_x.unwrap_or(0.0) - 10.0,
 			points_max_x: points_max_x.unwrap_or(0.0) + 10.0,
